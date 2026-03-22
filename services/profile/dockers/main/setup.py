b52f496873367b102b83e03141fb645ea4f2951c
hacker-volodya <3119142+hacker-volodya@users.noreply.github.com>
2026-03-22 20:26:51 +0300

    fix(services): add services registry for ructfe-2019

--- services/profile/dockers/main/setup.py
+++ services/profile/dockers/main/setup.py
@@ -5,10 +5,16 @@ from setuptools import find_packages, setup
 
 
 install_requires = [
-    'Flask',
-    'gunicorn',
-    'redis',
-    'requests'
+    'Flask<2',
+    'Jinja2<3',
+    'MarkupSafe<2',
+    'Werkzeug<2',
+    'certifi<2022',
+    'click<8',
+    'gunicorn<21',
+    'redis<4',
+    'requests<2.28',
+    'urllib3<2'
 ]
 
 setup(
